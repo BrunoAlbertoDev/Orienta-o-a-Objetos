fb4f3207553c9fdfe0908d51704bacd2594d1a3a
Bruno Alberto <tecrede81@gmail.com>
2025-08-13 15:25:29 -0300

    Terceira versão do Projeto 4

--- membros_estaticos/Calculator.java
+++ membros_estaticos/Calculator.java
@@ -2,14 +2,14 @@ package membros_estaticos;
 
 public class Calculator {
 
-    public final double PI = 3.14159; // está é uma constante
+    public static final double PI = 3.14159; // está é uma constante
 
-    public  double circunferencia(double radius){   // este é um método Estático
+    public static double circunferencia(double radius){   // este é um método Estático
 
         return  2.0 * PI * radius;
     }
 
-    public  double volume(double radius){            // este é um método Estático
+    public static double volume(double radius){            // este é um método Estático
 
         return  4.0 * PI * Math.pow(radius,2);
     }
--- membros_estaticos/Program.java
+++ membros_estaticos/Program.java
@@ -5,21 +5,23 @@ import java.util.Scanner;
 
 public class Program {
 
+
+
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Calculator calc = new Calculator();
+
 
         System.out.println("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = calc.circunferencia(radius);  // variável que chama um método Estático
-        double v = calc.volume(radius);         // variável que chama um método Estático
+        double c = Calculator.circunferencia(radius);  // variável que chama um método Estático
+        double v = Calculator.volume(radius);          // variável que chama um método Estático
 
         System.out.printf("Circuferencia = %.2f\n",c);
-        System.out.printf("Volume = %.2f\n",v);
-        System.out.printf("PI = %.2f\n",calc.PI);
+        System.out.printf("Volume = %.2f\n", v);
+        System.out.printf("PI = %.2f\n",Calculator.PI);
 
         sc.close();
 
